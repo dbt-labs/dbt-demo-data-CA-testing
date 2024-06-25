742afcaafd4f08fa9eaa79dfa75cbfe86d8b2da1
Stephen Younge <stephen.younge@fishtownanalytics.com>
2024-06-25 16:53:36 +0000

    added

---
+++ models/marts/customers_first_name_v3.sql
@@ -0,0 +1,7 @@
+with
+
+customers_no_first_name as (
+
+    select * from {{ ref('customers_v3') }}
+
+select *
--- models/stephenfakemodeljune2024.sql
+++ models/stephenfakemodeljune2024.sql
@@ -1,5 +1,4 @@
 with source as (select * from {{ source("ecommerce", "customers") }})
 
-select
-    split_part(name, ' ', 1) as first_name
+select id as customer_id, split_part(name, ' ', 1) as first_name
 from source
